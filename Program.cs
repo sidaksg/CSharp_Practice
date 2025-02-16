9d0f4aae44efd114884f5d21bee7a41f59a2cd39
HP <gujralsidak385@gmail.com>
2025-02-16 15:52:12 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Basic_OOPs;
+
+namespace C__Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BasicOops oops = new BasicOops();
+            //BasicOops oop = new BasicOops(100000);
+            //oop.Display();
+
+            //Console.WriteLine("Copy ctor");
+
+            //BasicOops oop1 = new BasicOops(oop);
+            //oop1.Display();
+
+
+            //oops.ShowDetails();
+            Console.WriteLine("Executing...");
+            Console.ReadKey();
+        }
+    }
+}
